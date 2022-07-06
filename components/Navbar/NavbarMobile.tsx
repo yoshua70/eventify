a58fd35f02587fc8c0d6ba70d89acf16a806c644
Michee Allidjinou <allidjinoumichee@outlook.fr>
2022-07-06 21:37:35 +0000

    feat: add menu icon

--- components/Navbar/NavbarMobile.tsx
+++ components/Navbar/NavbarMobile.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { NavbarLinks } from "./NavbarLinks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX, faBars } from "@fortawesome/free-solid-svg-icons";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 export const NavbarMobile = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -7,7 +10,11 @@ export const NavbarMobile = () => {
   return (
     <div className="flex flex-col md:hidden items-right">
       <p onClick={() => setIsVisible(!isVisible)} className="text-right">
-        {!isVisible ? "open" : "close"}
+        {!isVisible ? (
+          <FontAwesomeIcon icon={faBars} />
+        ) : (
+          <FontAwesomeIcon icon={faX} />
+        )}
       </p>
       <NavbarLinks isVisible={isVisible} className="flex flex-col items-end" />
     </div>
